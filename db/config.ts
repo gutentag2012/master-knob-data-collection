8b7915f1de51148c87c1f371ee3e37cc35870535
Joshua Gawenda <joshua.gawenda@gmail.com>
2025-12-28 17:31:00 +0100

    update timestamp retrieval

--- db/config.ts
+++ db/config.ts
@@ -61,7 +61,7 @@ const sensorData = defineTable({
     id: column.text({primaryKey: true}),
     session_task_id: column.text({references: () => sessionTask.columns.id}),
     timestamp: column.date({default: NOW}),
-    timestamp_ms: column.number({default: sql`EXTRACT(EPOCH FROM NOW()) * 1000`}),
+    timestamp_ms: column.number({default: sql`unixepoch('subsec')`}),
     timestamp_ms_string: column.text({default: sql`strftime('%s', 'now')`}),
     motor_angle: column.number({optional: true}),
     motor_angle_delta: column.number({optional: true}),
